eb364c4d3e50f7361a650312a4ce40a2a99eef5c
Lucas Oliveira <tilucasoli@gmail.com>
2020-09-16 10:23:28 -0300

    Add new groups following MVC

---
+++ SquirrelStorage/Model/Product.swift
@@ -0,0 +1,23 @@
+//
+//  Product.swift
+//  SquirrelStorage
+//
+//  Created by Lucas Oliveira on 16/09/20.
+//  Copyright © 2020 Lucas Oliveira. All rights reserved.
+//
+
+import UIKit
+
+struct Product {
+    let id: Int
+    let name: String
+    let image: URL
+    let qnty: Int
+    let costPrice: Double
+    let sellPrice: Double
+    let description: String
+    let tag: String
+    
+//    func getUIImage() -> UIImage {
+//    }
+}
