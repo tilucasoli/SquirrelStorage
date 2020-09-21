eb68da096bfdbdae10df6d5447a2aee9010b57bd
Rudigus <rodrigo.matos9@hotmail.com>
2020-09-21 10:37:05 -0300

    near finished screen

--- SquirrelStorage/Controller/ProductDetailView.swift
+++ SquirrelStorage/Controller/ProductDetailView.swift
@@ -30,15 +30,101 @@ class ProductDetailView: UIView {
         return quantityBar
     }()
     
+    let cardTitle: UILabel = {
+        let cardTitle = UILabel()
+        cardTitle.font = UIFont.systemFont(ofSize: 26, weight: .semibold)
+        cardTitle.textColor = .largeTitle
+        return cardTitle
+    }()
+    
+    let cardCategory: UILabel = {
+        let cardCategory = UILabel()
+        cardCategory.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        cardCategory.textColor = .smallText
+        return cardCategory
+    }()
+    
+    let starButton: UIButton = {
+        let starButton = UIButton(type: .custom)
+        let scaleConfig = UIImage.SymbolConfiguration(scale: .large)
+        starButton.setImage(UIImage(systemName: "star", withConfiguration: scaleConfig), for: .normal)
+        starButton.setImage(UIImage(systemName: "star.fill", withConfiguration: scaleConfig), for: .selected)
+        starButton.tintColor = .yellowSS
+        starButton.addTarget(self, action: #selector(starButtonPressed), for: .touchUpInside)
+        return starButton
+    }()
+    
+    let salePrice: UILabel = {
+        let salePrice = UILabel()
+        salePrice.font = UIFont.systemFont(ofSize: 26, weight: .bold)
+        salePrice.textColor = .darkPurpleSS
+        return salePrice
+    }()
+    
+    let costPrice: UILabel = {
+        let costPrice = UILabel()
+        costPrice.font = UIFont.systemFont(ofSize: 26, weight: .semibold)
+        costPrice.textColor = .largeTitle
+        return costPrice
+    }()
+    
+    let salePriceIndicator: UILabel = {
+        let salePriceIndicator = UILabel()
+        salePriceIndicator.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        salePriceIndicator.textColor = .smallText
+        salePriceIndicator.text = "Venda"
+        return salePriceIndicator
+    }()
+    
+    let costPriceIndicator: UILabel = {
+        let costPriceIndicator = UILabel()
+        costPriceIndicator.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        costPriceIndicator.textColor = .smallText
+        costPriceIndicator.text = "Custo"
+        return costPriceIndicator
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.font = UIFont.systemFont(ofSize: 22, weight: .semibold)
+        descriptionLabel.textColor = .largeTitle
+        descriptionLabel.text = "Descrição"
+        return descriptionLabel
+    }()
+    
+    let descriptionTextView: UITextView = {
+        let descriptionTextView = UITextView()
+        descriptionTextView.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        descriptionTextView.textColor = .smallText
+        descriptionTextView.backgroundColor = .background
+        descriptionTextView.textContainerInset = UIEdgeInsets.zero
+        descriptionTextView.textContainer.lineFragmentPadding = 0
+        descriptionTextView.isEditable = false
+        return descriptionTextView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupImageView()
-        setupDetailCard()
         setupQuantityBar()
+        setupDetailCard()
+        setupCardTitle()
+        setupCardCategory()
+        setupStarButton()
+        setupSalePrice()
+        setupCostPrice()
+        setupSalePriceIndicator()
+        setupCostPriceIndicator()
+        setupDescriptionLabel()
+        setupDescriptionTextView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func starButtonPressed() {
+        starButton.isSelected.toggle()
     }
     
     func setupImageView() {
@@ -48,7 +134,7 @@ class ProductDetailView: UIView {
             imageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             imageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
             imageView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: UIScreen.main.bounds.height * 0.41)
+            imageView.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 0.47)
         ])
     }
     
@@ -59,7 +145,7 @@ class ProductDetailView: UIView {
             detailCard.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -detailCard.layer.cornerRadius),
             detailCard.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
             detailCard.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
-            detailCard.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: UIScreen.main.bounds.height * 0.45 - detailCard.layer.cornerRadius)
+            detailCard.bottomAnchor.constraint(equalTo: quantityBar.topAnchor)
         ])
     }
     
@@ -67,11 +153,93 @@ class ProductDetailView: UIView {
         addSubview(quantityBar)
         quantityBar.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            quantityBar.topAnchor.constraint(equalTo: detailCard.bottomAnchor),
+            quantityBar.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 0.08),
             quantityBar.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
             quantityBar.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
             quantityBar.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         ])
     }
     
+    func setupCardTitle() {
+        addSubview(cardTitle)
+        cardTitle.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            cardTitle.leadingAnchor.constraint(equalTo: detailCard.leadingAnchor, constant: 16),
+            cardTitle.topAnchor.constraint(equalTo: detailCard.topAnchor, constant: 28)
+        ])
+    }
+    
+    func setupCardCategory() {
+        addSubview(cardCategory)
+        cardCategory.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            cardCategory.leadingAnchor.constraint(equalTo: cardTitle.leadingAnchor),
+            cardCategory.topAnchor.constraint(equalTo: cardTitle.bottomAnchor, constant: 4)
+        ])
+    }
+    
+    func setupStarButton() {
+        addSubview(starButton)
+        starButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            starButton.trailingAnchor.constraint(equalTo: detailCard.trailingAnchor, constant: -31),
+            starButton.topAnchor.constraint(equalTo: detailCard.topAnchor, constant: 32)
+        ])
+    }
+    
+    func setupSalePrice() {
+        addSubview(salePrice)
+        salePrice.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            salePrice.leadingAnchor.constraint(equalTo: detailCard.leadingAnchor, constant: 46),
+            salePrice.topAnchor.constraint(equalTo: cardCategory.bottomAnchor, constant: 32)
+        ])
+    }
+    
+    func setupCostPrice() {
+        addSubview(costPrice)
+        costPrice.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            costPrice.leadingAnchor.constraint(equalTo: detailCard.centerXAnchor, constant: 32),
+            costPrice.topAnchor.constraint(equalTo: cardCategory.bottomAnchor, constant: 32)
+        ])
+    }
+    
+    func setupSalePriceIndicator() {
+        addSubview(salePriceIndicator)
+        salePriceIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            salePriceIndicator.centerXAnchor.constraint(equalTo: salePrice.centerXAnchor),
+            salePriceIndicator.topAnchor.constraint(equalTo: salePrice.bottomAnchor, constant: 5)
+        ])
+    }
+    
+    func setupCostPriceIndicator() {
+        addSubview(costPriceIndicator)
+        costPriceIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            costPriceIndicator.centerXAnchor.constraint(equalTo: costPrice.centerXAnchor),
+            costPriceIndicator.topAnchor.constraint(equalTo: costPrice.bottomAnchor, constant: 5)
+        ])
+    }
+    
+    func setupDescriptionLabel() {
+        addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            descriptionLabel.leadingAnchor.constraint(equalTo: cardTitle.leadingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: salePriceIndicator.bottomAnchor, constant: 32)
+        ])
+    }
+    
+    func setupDescriptionTextView() {
+        addSubview(descriptionTextView)
+        descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            descriptionTextView.leadingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor),
+            descriptionTextView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 8),
+            descriptionTextView.trailingAnchor.constraint(equalTo: detailCard.trailingAnchor, constant: -16),
+            descriptionTextView.bottomAnchor.constraint(equalTo: detailCard.bottomAnchor, constant: -35)
+        ])
+    }
 }
--- SquirrelStorage/Controller/ProductDetailViewController.swift
+++ SquirrelStorage/Controller/ProductDetailViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class ProductDetailViewController: UIViewController {
     
+    var productDetailView: ProductDetailView! = nil
+    var product: Product?
+    
     override func loadView() {
-        view = ProductDetailView()
+        productDetailView = ProductDetailView()
+        view = productDetailView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Editar", style: .plain, target: self, action: nil)
-        view.backgroundColor = .systemRed
-        // Do any additional setup after loading the view.
+        productDetailView.backgroundColor = .systemRed
+        setupProductDetailView()
+    }
+    
+    func setupProductDetailView() {
+        productDetailView.cardTitle.text = "Título"
+        productDetailView.cardCategory.text = "Categoria"
+        productDetailView.salePrice.text = "R$1,00"
+        productDetailView.costPrice.text = "R$5,00"
+        //productDetailView.salePrice.text = "R$1"
+        //productDetailView.costPrice.text = "R$5"
+        productDetailView.descriptionTextView.text = "essa é uma descricao muito maneira de teste"
     }
 
 }
