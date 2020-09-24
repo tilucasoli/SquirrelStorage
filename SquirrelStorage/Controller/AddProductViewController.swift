a725b86d4faf3601d366d5b456d28e4ddc667556
lucasagosti <lucasagostif@gmail.com>
2020-09-24 16:55:59 -0300

    criando tela de adicionar produto e configurando botao de add

---
+++ SquirrelStorage/Controller/AddProductViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddProductViewController.swift
+//  SquirrelStorage
+//
+//  Created by Lucas Fernandes on 24/09/20.
+//  Copyright © 2020 Lucas Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class AddProductViewController: UIViewController {
+
+    var tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Salvar", style: .plain, target: self, action: nil)
+        
+        view.backgroundColor = .background
+        title = "Adicionar Produto"
+        navigationItem.largeTitleDisplayMode = .never
+    }
+}
+
+
+extension AddProductViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AddProduct", for: indexPath)
+        
+        return cell
+    }
+}
---
+++ SquirrelStorage/View/AddProductTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AddProductTableViewCell.swift
+//  SquirrelStorage
+//
+//  Created by Lucas Fernandes on 24/09/20.
+//  Copyright © 2020 Lucas Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class AddProductTableViewCell: UITableViewCell {
+
+
+}
